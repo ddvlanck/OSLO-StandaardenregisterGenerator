91e61bb3041a41d182ca25bf5812ee9ed39e60ef
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-02-27 15:19:47 +0100

    Updated body template Added cmd line option to provide a description for the page

--- html_page_generator.js
+++ html_page_generator.js
@@ -1,14 +1,16 @@
 const program = require('commander');
 const fs = require('fs');
 const nunjucks = require('nunjucks');
+const tmp = require('temp-write');
 
 
 
 program
-    .version('0.8.0')
+    .version('1.0.2')
     .usage('node html-generator.js creates html pages for json file of OSLOthema repositories')
     .option('-f, --file <target>', 'Location of configuration file of the repository for which template must be built')
-    .option('-o, --output <target>', 'Location where the HTML page should be saved');
+    .option('-o, --output <target>', 'Location where the HTML page should be saved')
+    .option('-d, --description <target>', 'Location of the HTML page that contains the description of the repo in the standards register');
 
 program.on('--help', function(){
     console.log('')
@@ -24,7 +26,13 @@ if(output.charAt(output.length-1) === '/'){
 const content = fs.readFileSync(program.file);
 const configuration = JSON.parse(content.toString());
 
-nunjucks.configure('./templates', {
+if(program.description){
+    const description = fs.readFileSync(program.description.toString()).toString();
+    const tempFilePath = tmp.sync(description);
+    configuration.description = tempFilePath;
+}
+
+nunjucks.configure(['./templates', configuration.description], {
     autoescape: true
 });
 
@@ -37,5 +45,5 @@ fs.writeFile(output, data, (err) => {
         process.exitCode = 1
         throw err;
     }
-    console.log('The file has been saved to standaarden_pagina.html');
+    console.log('The file has been saved to ' + output);
 });
